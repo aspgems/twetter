8517af66d8883f33fbf81fa7e4b2aec115c477de
javier <javier.ramirez.gomara@gmail.com>
2010-09-29 18:00:47 +0200

    html title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
     def setup(options)
-        @title = "Twetter / #{options[:title]}" || "Twetter"
+        @title = "Jester - ASPgems / #{options[:title]}" || "Twetter"
         @body_id = options[:body_id] || "body"
         @body_classes = options[:body_classes] || "account"
         @css = options[:css] || []
