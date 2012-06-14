c1f39276d647224795686bf7dc46cecd61af5ff5
Diego Rodriguez <diec123@yahoo.es>
2012-06-14 12:18:33 +0200

    Added bundler

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '2.1.2'
+gem 'sqlite3'
+
+gem 'twitter', '1.1.2'
--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,6 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
-  config.gem "twitter", :version => '1.1.2'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
